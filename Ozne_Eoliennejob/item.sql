a2bd6d58af6f22572b79b59b5188eb68bf0cd319
Ozne <57328206+Ozneeee@users.noreply.github.com>
2020-06-07 11:52:29 +0200

    Add files via upload

---
+++ Ozne_Eoliennejob/item.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('hatchet_lj', 'Outil de Réparation', 1, 0, 1),
+('wood', 'Morceau d Eolienne', 100, 0, 1),
+('pro_wood', 'Ticket Salaire', 100, 0, 1);
